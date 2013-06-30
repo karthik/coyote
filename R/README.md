545c1fbceacc24386dc100d1d6d9364a447dc795
Karthik Ram <karthik.ram@gmail.com>
2013-06-30 12:11:07 -0700

    Added intital documentation and a README

---
+++ R/README.md
@@ -0,0 +1,14 @@
+
+# Coyote
+
+In an effort to keep my entire R environment reproducible, I'm moving everything (except API keys) into here. 
+
+Install with: 
+
+```coffee
+install_github("coyote", "karthikram")
+```
+
+## Bugs  
+
+Please file bugs as issues. 
--- R/check_packages.R
+++ R/check_packages.R
@@ -1,14 +1,11 @@
-
-#'<brief desc>
+#'Check and reports if any of your packages are out of date with CRAN
 #'
-#'<full description>
-#' @param  <what param does>
 #' @export
 #' @examples \dontrun{
-#'
+#' check.packages()
 #'}
 check.packages <- function() {
-    if (!is.null(utils::old.packages())) {
+    if (!is.null(utils::old.packages())) {	
         old_packages <- utils::old.packages()
         cat("Notification:", dim(old_packages)[1], "packages are out of date \n")
         cat(unname(old_packages[, 1]), sep = ",","\n")
