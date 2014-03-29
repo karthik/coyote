caa7c1aab400eb856fd1377ce3163887323ec4db
Karthik Ram <karthik.ram@gmail.com>
2014-03-29 10:52:24 -0700

    No longer loading plyr and reshape in startup. Just gets me in trouble.

--- R/startup.R
+++ R/startup.R
@@ -1,24 +1,8 @@
 #' @noRd
 .onAttach <- function() {
-
-	  suppressPackageStartupMessages(library(ggplot2))
-    suppressPackageStartupMessages(library(reshape2))
-    suppressPackageStartupMessages(library(plyr))
     suppressPackageStartupMessages(library(knitr))
     suppressPackageStartupMessages(library(devtools))
-
-# These currently don't execute. need to move to Onload
-theme_minimal_light <- function (base_size = 12, base_family = "", ...){
-  modifyList (theme_minimal (base_size = base_size, base_family = base_family),
-              list (axis.ticks = element_line (colour = "grey50"),
-                    axis.text.x = theme_text (colour = "grey33"),
-                    axis.text.y = theme_text (colour = "grey33")))
-}
-
-theme_minimal_cb_L <- function (base_size = 12, base_family = "", ...){
-  modifyList (theme_minimal (base_size = base_size, base_family = base_family),
-              list (axis.line = element_line (colour = "black")))
-}
+    suppressPackageStartupMessages(library(ggplot2))
 
 # I prefer that all my plots have a minimalist theme. So I keep this as default in my .profile
 if (require(ggplot2, quietly = TRUE)) {
